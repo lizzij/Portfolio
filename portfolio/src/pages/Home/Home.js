314028419c075f94bd8c1d75dbed135cadc915f7
eliza <elizazijinhuang@gmail.com>
2020-07-30 21:33:22 -0400

    wrap text

--- portfolio/src/pages/Home/Home.js
+++ portfolio/src/pages/Home/Home.js
@@ -44,8 +44,8 @@ function Home() {
         <div className="VideoPlayer">
           <iframe id="video-iframe"
             data-module-vimeo-player="video-promo"
-            src="https://player.vimeo.com/video/442765402?controls=0&amp;autoplay=1&amp;
-            amptitle=0&amp;byline=0&amp;portrait=0&amp;sidedock=0;&amp;
+            src="https://player.vimeo.com/video/442765402?background=1&amp;controls=0&amp;
+            autoplay=1&amp;amptitle=0&amp;byline=0&amp;portrait=0&amp;sidedock=0;&amp;
             transparent=false&amp;autopause=false&amp;loop=1&amp;muted=1" 
             width="100%" height="100%" data-vimeo-player-autoplay="true" 
             frameBorder="0" webkitallowfullscreen="" mozallowfullscreen="" allowFullScreen="" 
