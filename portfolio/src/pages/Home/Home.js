cfa25eb8f573ff7ee492725a0c199012ac29a96a
eliza <elizazijinhuang@gmail.com>
2020-07-31 10:42:45 -0400

    finetune marquee details

--- portfolio/src/pages/Home/Home.js
+++ portfolio/src/pages/Home/Home.js
@@ -99,20 +99,20 @@ function Home() {
         <p>More Content Coming Soon. I am working on it.</p>
       </div>
 
-      <p className="marquee">
+      <p className="Marquee">
         <span>
-          sp thx to my doggo&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
-          crt to my rubber duck&nbsp;[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈﹏⚈]&nbsp;
-          Bye covid&nbsp;[⚈  ͟ʖ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ▿ ⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈ ω⚈]&nbsp;
-          Brutalism? OCISLY ❥&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
-          import tf as np&nbsp;[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈ ω⚈]&nbsp;
+          <nobr className="MinorBlurb">special thx to my doggo</nobr>&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
+          <nobr className="MinorBlurb">credits to my rubber duck&nbsp;</nobr>[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈﹏⚈]&nbsp;
+          <nobr className="MinorBlurb">Bye covid!</nobr>&nbsp;[⚈  ͟ʖ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ▿ ⚈]&nbsp;Design &amp; Code w  ❥ in figma &amp; react&nbsp;[⚈ ω⚈]&nbsp;
+          <nobr className="MinorBlurb">Brutalism? OCISLY ❥ </nobr>&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
+          <nobr className="MinorBlurb">import tf as np</nobr>&nbsp;[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈ ω⚈]&nbsp;
         </span>
         <span>
-          sp thx to my doggo&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
-          crt to my rubber duck&nbsp;[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈﹏⚈]&nbsp;
-          Bye covid&nbsp;[⚈  ͟ʖ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ▿ ⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈ ω⚈]&nbsp;
-          Brutalism? OCISLY ❥&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
-          import tf as np&nbsp;[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈ ω⚈]&nbsp;
+          <nobr className="MinorBlurb">special thx to my doggo</nobr>&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
+          <nobr className="MinorBlurb">credits to my rubber duck&nbsp;</nobr>[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈﹏⚈]&nbsp;
+          <nobr className="MinorBlurb">Bye covid!</nobr>&nbsp;[⚈  ͟ʖ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ▿ ⚈]&nbsp;Design &amp; Code w  ❥ in figma &amp; react&nbsp;[⚈ ω⚈]&nbsp;
+          <nobr className="MinorBlurb">Brutalism? OCISLY ❥ </nobr>&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
+          <nobr className="MinorBlurb">import tf as np</nobr>&nbsp;[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈ ω⚈]&nbsp;
         </span>
       </p>
     </div>
