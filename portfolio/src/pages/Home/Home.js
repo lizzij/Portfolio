a96a5dd168fce13046f29deb8dc616ce2c249a63
eliza <elizazijinhuang@gmail.com>
2020-08-01 14:47:16 -0400

    move out everything

--- portfolio/src/pages/Home/Home.js
+++ portfolio/src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import Header from '../../components/Header'
 import Video from '../../components/Video';
 import Projects from '../../components/Projects';
 import ComingSoon from '../../components/ComingSoon';
+import Footer from '../../components/Footer';
 import './Home.scss';
 
 function Home() {
@@ -13,25 +14,9 @@ function Home() {
       <NavBar/>
       <Header/>
       <Video/>
-      <Projects />
-      <ComingSoon />
-
-      <p className="Marquee">
-        <span>
-          <nobr className="MinorBlurb">special thx to my doggo</nobr>&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
-          <nobr className="MinorBlurb">credits to my rubber duck&nbsp;</nobr>[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈﹏⚈]&nbsp;
-          <nobr className="MinorBlurb">Bye covid!</nobr>&nbsp;[⚈  ͟ʖ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ▿ ⚈]&nbsp;Design &amp; Code w  ❥ in figma &amp; react&nbsp;[⚈ ω⚈]&nbsp;
-          <nobr className="MinorBlurb">Brutalism? OCISLY ❥ </nobr>&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
-          <nobr className="MinorBlurb">import tf as np</nobr>&nbsp;[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈ ω⚈]&nbsp;
-        </span>
-        <span>
-          <nobr className="MinorBlurb">special thx to my doggo</nobr>&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
-          <nobr className="MinorBlurb">credits to my rubber duck&nbsp;</nobr>[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈﹏⚈]&nbsp;
-          <nobr className="MinorBlurb">Bye covid!</nobr>&nbsp;[⚈  ͟ʖ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ▿ ⚈]&nbsp;Design &amp; Code w  ❥ in figma &amp; react&nbsp;[⚈ ω⚈]&nbsp;
-          <nobr className="MinorBlurb">Brutalism? OCISLY ❥ </nobr>&nbsp;[⚈ ᵜ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈ ₃⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈‿⚈]&nbsp;
-          <nobr className="MinorBlurb">import tf as np</nobr>&nbsp;[⚈ ᴥ ⚈]&nbsp;Eliza Huang @ 2020&nbsp;[⚈◡⚈]&nbsp;Design &amp; Code w  ❥ in figma + react&nbsp;[⚈ ω⚈]&nbsp;
-        </span>
-      </p>
+      <Projects/>
+      <ComingSoon/>
+      <Footer/>
     </div>
   );
 }
