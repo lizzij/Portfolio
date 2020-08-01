b923af5f4623c80a8c69b94fa2a1f94767985d28
eliza <elizazijinhuang@gmail.com>
2020-08-01 17:21:26 -0400

    navigation

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/styles.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Home from './pages/Home/Home';
+import Contact from './pages/Contact/Contact';
+import About from './pages/About/About';
 import * as serviceWorker from './serviceWorker';
+import './assets/styles/styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ portfolio/src/pages/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../../components/NavBar'
+import './About.scss';
+
+function About() {
+
+  return (
+    <div className="About">
+      <NavBar/>
+      <div>About</div>
+    </div>
+  );
+}
+
+export default About;
