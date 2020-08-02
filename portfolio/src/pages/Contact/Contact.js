a919a2048be4a559920026e5718d03ef43be63ab
eliza <elizazijinhuang@gmail.com>
2020-08-02 11:03:16 -0400

    add hyperlinks

--- portfolio/src/pages/Contact/Contact.js
+++ portfolio/src/pages/Contact/Contact.js
@@ -15,38 +15,47 @@ function Contact() {
     <div className="Contact">
       <NavBar navStatus={ navStatus } />
       <Container fluid>
-        <Row className="ContactRow noGutters border-top">
-          <Col>
-            <span className="Brand">eliza</span>
-            <span className="Light">zijin</span>
-            <span className="Brand">huang</span>
-            <span className="Light">@</span>
-            <span className="Dark">gmail</span>
-            <span className="Light">.com</span>
+        <a href="mailto:elizazijinhuang@gmail.com" rel="noopener noreferrer" target="_blank"> 
+          <Row className="ContactRow noGutters border-top">
+            <Col>
+              <span className="Brand">eliza</span>
+              <span className="Light">zijin</span>
+              <span className="Brand">huang</span>
+              <span className="Light">@</span>
+              <span className="Dark">gmail</span>
+              <span className="Light">.com</span>
+              </Col>
+            <Col xs lg="1">
+              <Image src={EmailIcon} className="Icons"/>
             </Col>
-          <Col xs lg="1">
-            <Image src={EmailIcon} className="Icons"/>
-          </Col>
-        </Row>
-        <Row className="ContactRow noGutters border-top">
-          <Col>
-            <span className="Dark">linkedin</span>
-            <span className="Light">.com/in/</span>
-            <span className="Brand">lizzij</span>
+          </Row>
+        </a>
+
+        <a href="https://www.linkedin.com/in/lizzij/" rel="noopener noreferrer" target="_blank">
+          <Row className="ContactRow noGutters border-top">
+            <Col>
+              <span className="Dark">linkedin</span>
+              <span className="Light">.com/in/</span>
+              <span className="Brand">lizzij</span>
+              </Col>
+            <Col xs lg="1">
+              <Image src={LinkedinIcon} className="Icons"/>
             </Col>
-          <Col xs lg="1">
-            <Image src={LinkedinIcon} className="Icons"/>
-          </Col>
-        </Row>
-        <Row className="ContactRow noGutters border-top">
-          <Col>
-            <span className="Dark">github</span>
-            <span className="Light">.com/</span>
-            <span className="Brand">lizzij</span></Col>
-          <Col xs lg="1">
-            <Image src={GithubIcon} className="Icons"/>
-          </Col>
-        </Row>
+          </Row>
+        </a>
+
+        <a href="https://github.com/lizzij" rel="noopener noreferrer" target="_blank">
+          <Row className="ContactRow noGutters border-top">
+            <Col>
+              <span className="Dark">github</span>
+              <span className="Light">.com/</span>
+              <span className="Brand">lizzij</span></Col>
+            <Col xs lg="1">
+              <Image src={GithubIcon} className="Icons"/>
+            </Col>
+          </Row>
+        </a>
+        
         <Row className="ContactRow noGutters border-top border-bottom">
           <Col>
             <span className="Brand">eliza</span>
