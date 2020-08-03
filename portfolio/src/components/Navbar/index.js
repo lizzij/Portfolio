110ea89ce84b2dc37071a4eb67673de3d5617ce8
eliza <elizazijinhuang@gmail.com>
2020-08-03 13:27:46 -0400

    change color details

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -5,16 +5,21 @@ import './styles.scss';
 export default class NavBar extends Component {
     render() {
         return (
-            <Navbar expand="lg" className="NavBar fixed-top">
+            <Navbar expand="lg" className="NavBar fixed-top d-flex flex-row justify-content-between">
                 <Navbar.Brand className="LeftNavlink" href="/">lizzij</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="d-flex flex-row justify-content-end">
+                {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+                {/* <Navbar.Collapse id="basic-navbar-nav" className="d-flex flex-row justify-content-end">
                     <Nav className="d-flex flex-row">
                         <Nav.Link className={`RightNavlink sm-auto ${this.props.navStatus.projects}`} href="/">Projects</Nav.Link>
                         <Nav.Link className={`RightNavlink sm-auto ${this.props.navStatus.contact}`} href="/contact">Contact</Nav.Link>
                         <Nav.Link className={`RightNavlink sm-auto ${this.props.navStatus.about}`} href="/about">About</Nav.Link>
                     </Nav>
-                </Navbar.Collapse>
+                </Navbar.Collapse> */}
+                <Nav className="d-flex flex-row">
+                    <Nav.Link className={`RightNavlink sm-auto ${this.props.navStatus.projects}`} href="/">Projects</Nav.Link>
+                    <Nav.Link className={`RightNavlink sm-auto ${this.props.navStatus.contact}`} href="/contact">Contact</Nav.Link>
+                    <Nav.Link className={`RightNavlink sm-auto ${this.props.navStatus.about}`} href="/about">About</Nav.Link>
+                </Nav>
             </Navbar>
         );
     }
