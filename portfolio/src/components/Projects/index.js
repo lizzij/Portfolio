df5d2e74fcfc837cc805328576e0032dd3cb824e
eliza <elizazijinhuang@gmail.com>
2020-09-13 14:17:24 -0400

    containerize tags

--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Button } from 'react-bootstrap';
+import ProjectTag from '../../components/ProjectTag'
 import './styles.scss';
 
 const Projects = () => {
@@ -7,36 +8,16 @@ const Projects = () => {
     <section className="Projects">
         <Container fluid className="Content">
           <Row className="Tags">
-            <Button variant="outline-dark" className="Pill">
-              <div className="Label">selected</div>
-            </Button>
-            <Button variant="outline-dark" className="Pill">
-              <div className="Label">all</div>
-            </Button>
-            <Button variant="outline-dark" className="Pill">
-              <div className="Label">web dev</div>
-            </Button>
-            <Button variant="outline-dark" className="Pill">
-              <div className="Label">ml</div>
-            </Button>
-            <Button variant="outline-dark" className="Pill">
-              <div className="Label">hackathon</div>
-            </Button>
-            <Button variant="outline-dark" className="Pill">
-              <div className="Label">coop</div>
-            </Button>
-            <Button variant="outline-dark" className="Pill">
-              <div className="Label">research</div>
-            </Button>
-            <Button variant="outline-dark" className="Pill">
-              <div className="Label">mooc</div>
-            </Button>
-            <Button variant="outline-dark" className="Pill">
-              <div className="Label">blog</div>
-            </Button>
-            <Button variant="outline-dark" className="Pill">
-              <div className="Label">etc</div>
-            </Button>
+            <ProjectTag label="selected" />
+            <ProjectTag label="all" />
+            <ProjectTag label="web dev" />
+            <ProjectTag label="ml" />
+            <ProjectTag label="hackathon" />
+            <ProjectTag label="coop" />
+            <ProjectTag label="research" />
+            <ProjectTag label="mooc" />
+            <ProjectTag label="blog" />
+            <ProjectTag label="etc" />
           </Row>
         </Container>
     </section>
