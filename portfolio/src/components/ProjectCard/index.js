e6fc155e8cfe62912e841a1bea7fbda6f2de4862
eliza <elizazijinhuang@gmail.com>
2020-09-16 13:47:30 -0400

    format project cards

--- portfolio/src/components/ProjectCard/index.js
+++ portfolio/src/components/ProjectCard/index.js
@@ -5,9 +5,38 @@ import './styles.scss';
 const ProjectCard = ({ index, project }) => {
     // eslint-disable-next-line
     const {title, tags, image, blurb, readme} = project;
+    var format;
+    switch(index % 8) {
+        case 0:
+            format = "col-md-4";
+            break;
+        case 1:
+            format = "col-md-6 offset-md-6 moveUp";
+            break;
+        case 2:
+            format = "col-md-6 offset-md-3";
+            break;
+        case 3:
+            format = "col-md-9";
+            break;
+        case 4:
+            format = "col-md-4 offset-md-8";
+            break;
+        case 5:
+            format = "col-md-6 moveUp";
+            break;
+        case 6:
+            format = "col-md-6 offset-md-3";
+            break;
+        case 7:
+            format = "col-md-9";
+            break;
+        default:
+            format = "col-md-4";
+    }
     return (
         <Row className="ProjectCardRow">
-            <Col className={`ProjectCard ${index % 8} col-md-6 offset-md-3`}>
+            <Col className={`ProjectCard ${format}`}>
                 <Image src={image} fluid className="Thumbnail"/>
                 <div className="Title">{title}</div>
                 <div className="Abstract">
