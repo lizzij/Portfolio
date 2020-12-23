a4568056db59a7ead8782045ec341690215b75b2
eliza <elizazijinhuang@gmail.com>
2020-12-23 11:20:28 -0500

    project component

--- portfolio/src/components/Work/index.js
+++ portfolio/src/components/Work/index.js
@@ -1,37 +1,27 @@
 import React from 'react';
 import { Collapse } from 'antd';
+import { projects } from './data.json'
 import './styles.css';
 
 const { Panel } = Collapse;
 
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
+const Project = ({ project }) => {
+  const { header, key, blurb } = project;
+  return (
+    <Panel header={header} key={key}>
+      <p>{blurb}</p>
+    </Panel>
+  );
+}
 
-function Work() {
+const Work = () => {
   return (
     <Collapse 
       accordion 
       // defaultActiveKey={['1']}
       expandIconPosition="right"
     >
-      <Panel header="This is panel header 1" key="1">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 2" key="2">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 3" key="3">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 4" key="4">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 5" key="5">
-        <p>{text}</p>
-      </Panel>
+      {projects.map((project) => <Project project={project} />)}
   </Collapse>
   );
 }
