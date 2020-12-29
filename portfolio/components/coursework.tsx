155b71cdb34a6254bf03e39b117a5ab7f01b65e9
eliza <elizazijinhuang@gmail.com>
2020-12-29 18:02:57 -0500

    responsive

--- portfolio/components/coursework.tsx
+++ portfolio/components/coursework.tsx
@@ -4,27 +4,24 @@ const Coursework = () => {
   return (
     <div>
       <SectionHeader header="coursework" />
-      <div className="flex flex-wrap place-content-between">
-        <div className="flex-1 mr-4 lg:mr-16 bg-gray-200">
-          <div className="grid grid-cols-1 divide-y divide-black">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 lg:gap-16">
+        <div className="grid grid-cols-1 divide-y divide-black">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div></div>
         </div>
-         <div className="flex-1 mr-4 lg:mr-16 bg-gray-200">
-          <div className="grid grid-cols-1 divide-y divide-black">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
+        <div className="grid grid-cols-1 divide-y divide-black">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div></div>
         </div>
-        <div className="flex-1 bg-gray-200">
-          <div className="grid grid-cols-1 divide-y divide-black">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
+        <div className="grid grid-cols-1 divide-y divide-black">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div></div>
         </div>
       </div>
     </div>
--- portfolio/components/experience.tsx
+++ portfolio/components/experience.tsx
@@ -4,14 +4,14 @@ const Experience = () => {
   return (
     <div>
       <SectionHeader header="experience" />
-      <div className="flex flex-wrap place-content-between">
-        <div className="flex-1 mr-4 lg:mr-16 bg-gray-200">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-16">
+        <div className="bg-gray-200 rounded-2xl md:rounded-3xl p-4">
           <div>1</div>
-          <div>2</div>
+          <div>Donec vel iaculis neque. Quisque semper sapien in justo convallis, ac consequat risus pellentesque. Vestibulum porttitor mauris eget tempus ullamcorper. Ut varius</div>
         </div>
-        <div className="flex-1 bg-gray-200">
+        <div className="bg-gray-200 rounded-2xl md:rounded-3xl p-4">
           <div>1</div>
-          <div>2</div>
+          <div>Donec vel iaculis neque. Quisque semper sapien in justo convallis, ac consequat risus pellentesque. Vestibulum porttitor mauris eget tempus ullamcorper. Ut varius</div>
         </div>
       </div>
     </div>
