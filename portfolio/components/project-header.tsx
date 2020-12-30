288e87dcfb2bdd64f785400b8057afa5455e5e4c
eliza <elizazijinhuang@gmail.com>
2020-12-30 11:37:42 -0500

    format post

--- portfolio/components/project-header.tsx
+++ portfolio/components/project-header.tsx
@@ -14,9 +14,6 @@ const ProjectHeader = ({ title, coverImage, date, author }: Props) => {
   return (
     <>
       <ProjectTitle>{title}</ProjectTitle>
-      <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} />
-      </div>
       <div className="max-w-2xl mx-auto">
         <div className="mb-6 text-lg">
           <DateFormatter dateString={date} />
