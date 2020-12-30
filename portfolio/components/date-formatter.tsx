a68f4a2ba5e8617b8ebc7b056a04623fe91951e0
eliza <elizazijinhuang@gmail.com>
2020-12-30 10:45:01 -0500

    date formatter

--- portfolio/components/date-formatter.tsx
+++ portfolio/components/date-formatter.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const DateFormatter = ({ dateString }: Props) => {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  return <div className="text-gray-600 md:pb-1"><time dateTime={dateString}>{format(date, 'yyyy.MM.dd')}</time></div>
 }
 
 export default DateFormatter
