85c56e90886b2a9dfeb9625602b60389e47b2752
eliza <elizazijinhuang@gmail.com>
2020-12-31 22:15:49 -0500

    better accessibility

--- portfolio/components/experience.tsx
+++ portfolio/components/experience.tsx
@@ -7,8 +7,8 @@ const Experience = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-16">
         <div className="bg-gray-100 rounded-2xl md:rounded-3xl p-4">
           <div className="md:text-xl text-lg pb-1">Software Engineer Coop at Harvard Catalyst</div>
-          <div className="text-gray-600 pb-1">2020.6 - 2020.12</div>
-          <div className="text-gray-500">
+          <div className="text-gray-900 pb-1">2020.6 - 2020.12</div>
+          <div className="text-gray-800">
             I worked as a full-stack intern on the data network team. 
             I collaborated with a team of developers, product managers and testers
             in an agile method - to add new features, fix bugs and reduce technical debt
@@ -17,8 +17,8 @@ const Experience = () => {
         </div>
         <div className="bg-gray-100 rounded-2xl md:rounded-3xl p-4">
           <div className="md:text-xl text-lg pb-1">Software Engineer Coop at Broad Institute</div>
-          <div className="text-gray-600 pb-1">2019.6 - 2019.12</div>
-          <div className="text-gray-500">
+          <div className="text-gray-900 pb-1">2019.6 - 2019.12</div>
+          <div className="text-gray-800">
             I worked as a full-stack intern on the tooling team. 
             I developed a web-based analysis tool to search and annotate a particular type of 
             genetic variants that causes rare diseases. 
